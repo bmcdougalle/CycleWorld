4fcfa60564ce3a8af00be4c1b6988e5860cc2efa
Luke <lukereibel@gmail.com>
2021-02-16 18:36:40 -0500

    luke first pushhhh

---
+++ CycleWorld.Data/Bike.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleWorld.Data
+{
+    public class Bike
+    {
+        [Key]
+        public int BikeId { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Make { get; set; }
+
+
+    }
+}

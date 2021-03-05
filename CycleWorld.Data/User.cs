eb141fb0a474f5e4a04fa7c4f7dea1f6462949b4
jasondavis <jasondavis925@gmail.com>
2021-03-04 21:11:04 -0500

    v0.1

---
+++ CycleWorld.Data/User.cs
@@ -0,0 +1,34 @@
+﻿using CycleShops.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleWorld.Data
+{
+    public class User
+    {
+
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public Guid PersonalId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        // public List<Bike> ListOfBikes { get; set; }
+
+        [ForeignKey(nameof(Shop))]
+        public int ShopId { get; set; }
+        public virtual Shop Shop {get; set;}
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
